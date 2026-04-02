ae547e4079d10e50b86ec1cbe80d82bcff7d44ed
Akash kumar <akashkumar@Akashs-MacBook-Air.local>
2026-04-03 01:06:02 +0530

    Add and fix loop.java and add ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+
+
+    public static void main(String[] args) {
+        int salary = 25000;
+
+        if (salary > 10000) {
+            salary = salary + 2000;
+        }
+        System.out.println(salary);
+    }
+}
